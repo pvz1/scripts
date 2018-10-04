e308e2fb542c72740a7daf21b37e16944a9645bb
pvz1 <41412941+pvz1@users.noreply.github.com>
2018-10-04 14:33:37 +0530

    Update redis-monitoring-setup.sh

--- redis-monitoring-setup.sh
+++ redis-monitoring-setup.sh
@@ -128,8 +128,6 @@ main "$@"
 
 ##################################################################################################
 
-apt-get --yes install libhiredis0.13
-
 (cd /opt/stackdriver/collectd/etc/collectd.d/ \
   && curl -O "https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/redis.conf")
 
