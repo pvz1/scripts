2c9e923639e6574cd862cb12107c4cf05895ae42
pvz1 <41412941+pvz1@users.noreply.github.com>
2018-10-08 16:48:28 +0530

    Update run-redis-server.sh

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis_ip_correction.sh
 sleep 2s
-sh ./redis_ip_correction.sh &
-sleep 2s
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis-monitoring-setup.sh
 sleep 2s
 sh ./redis_ip_correction.sh &
