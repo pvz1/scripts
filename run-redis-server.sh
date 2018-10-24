a599c2577400282ac4840179d3626f511d98663a
pvz1 <41412941+pvz1@users.noreply.github.com>
2018-10-24 15:18:26 +0530

    Update run-redis-server.sh

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -3,11 +3,6 @@ wget https://raw.githubusercontent.com/pvz1/scripts/master/redis_ip_correction.s
 #sleep 5s
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis-monitoring-setup.sh
 #sleep 2s
-apt-get update
-sleep 3s
-apt-get --yes install curl
-apt-get --yes install lsb-release
-
 sh ./redis_ip_correction.sh &
 sh ./redis-monitoring-setup.sh &
 #sleep 30s
