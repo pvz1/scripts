32e9c74bb5561761388ff072402ee40dfadbe843
pvz1 <41412941+pvz1@users.noreply.github.com>
2018-10-24 16:34:25 +0530

    Update run-redis-server.sh
    Final script to install stackdriver agent on redis cluster pods.

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis_ip_correction.sh
-#sleep 5s
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis-monitoring-setup.sh
-#sleep 2s
 sh ./redis_ip_correction.sh &
 sh ./redis-monitoring-setup.sh &
-#sleep 30s
 redis-server /conf/redis.conf 
-#sleep 5s
+
 
