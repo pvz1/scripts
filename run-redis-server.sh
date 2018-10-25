da4063aa839a4c113edbad1b6d8cfc4d3354abf7
pvz1 <41412941+pvz1@users.noreply.github.com>
2018-10-25 15:48:40 +0530

    Update run-redis-server.sh
    Added comments

--- run-redis-server.sh
+++ run-redis-server.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis_ip_correction.sh
 wget https://raw.githubusercontent.com/pvz1/scripts/master/redis-monitoring-setup.sh
+# Redis monitor script should run after the redis-server setup to successfully send the metrics to stack driver agent. 
+# But when we run both scripts in foreground redis-server setup interupts monitor script and fails the setup. 
+# Due to this issue we are running the monitor script in background and redis-server run command in foreground. And also kubernetes pods keep restarts if there is no foreground process is running in it. 
 sh ./redis_ip_correction.sh &
 sh ./redis-monitoring-setup.sh &
 redis-server /conf/redis.conf 
